0083e3366a274618dad2d9ed745763be53d159ab
Marcello Brasileiro <brasileiro.marcello@hotmail.com>
2024-06-30 11:50:18 -0400

    inserindo a logo do site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import logo from './imagens/logo.svg'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        
+        <div className='logo'>
+          <img src={logo}></img>
+        </div>
       </header>
     </div>
   );
